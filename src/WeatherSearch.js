ac151e7bc82f0722304dbe129009861399e30f0a
sahra001 <134542621+sahra001@users.noreply.github.com>
2023-12-18 21:04:28 +0430

    Add files via upload

---
+++ src/WeatherSearch.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input type="search" placeholder="Enter a city.." />
+        <button type="Submit">Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
